99e159a162976ef50e85077b929d9e991f7fd6fb
Nina Bobyleva <2847171@mail.ru>
2024-04-19 22:12:23 +0300

    add file data.js

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const cardList = [
+    {
+        id: 1,
+        topic: 'Web Design',
+        title: 'Название задачи',
+        date: '30.10.23',
+        status: 'Без статуса',
+    },
+    {
+        id: 1,
+        topic: 'Research',
+        title: 'Название задачи',
+        date: '20.02.23',
+        status: 'Нужно сделать',
+    },
+    {
+        id: 1,
+        topic: 'Copywriting',
+        title: 'Название задачи',
+        date: '01.10.23',
+        status: 'В работе',
+    },
+    {
+        id: 1,
+        topic: 'Research',
+        title: 'Название задачи',
+        date: '30.01.24',
+        status: 'Тестирование',
+    },
+    {
+        id: 1,
+        topic: 'Copywriting',
+        title: 'Название задачи',
+        date: '30.02.24',
+        status: 'Готово',
+    },
+]
