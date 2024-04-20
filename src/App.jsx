2a933c7a87f3e4671f2979dae742ce519d220d86
Nina Bobyleva <2847171@mail.ru>
2024-04-20 20:55:52 +0300

    add PopExit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PopBrowse from './components/popups/PopBrowse/PopBrowse';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
 import { cardList } from './data';
+import PopExit from './components/popups/PopExit/PopExit';
 
 function App() {
 	const [cards, setCards] = useState(cardList);
@@ -28,35 +29,15 @@ function App() {
 			setIsLoading(false);
 		}, 1000)
 	}, [])
-	console.log(isLoading);
+	
 	return (
-		<>
 		<div className="wrapper">
-				<div className="pop-exit" id="popExit">
-					<div className="pop-exit__container">
-						<div className="pop-exit__block">
-							<div className="pop-exit__ttl">
-								<h2>Выйти из аккаунта?</h2>
-							</div>
-							<form className="pop-exit__form" id="formExit" action="#">
-								<div className="pop-exit__form-group">
-									<button className="pop-exit__exit-yes _hover01" id="exitYes"><a href="modal/signin.html">Да, выйти</a> </button>
-									<button className="pop-exit__exit-no _hover03" id="exitNo"><a href="main.html">Нет, остаться</a> </button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-
-				<PopNewCard />
-
-				<PopBrowse />
-
+			<PopExit />
+			<PopNewCard />
+			<PopBrowse />
 			<Header addCard={addCard}/>
 			<Main cards={cards} isLoading={isLoading}/>
-			
 		</div>
-		</>
 	)
 }
 
