36e0733fff25fe71cbed5bb7ef02f1076a8836e4
Nina Bobyleva <2847171@mail.ru>
2024-06-02 00:52:05 +0300

    correction

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
-import './index.css'
 import { UserProvider } from './context/userContext.jsx';
 import { CardsProvider } from './context/cardContext.jsx';
 import "react-day-picker/dist/style.css";
